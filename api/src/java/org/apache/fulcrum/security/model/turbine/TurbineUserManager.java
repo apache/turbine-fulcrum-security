559ad5af9138dc0a6fa75ca117b687efad7d9ca9
Georg Kallidis <gk@apache.org>
2017-04-06 14:25:49 +0000

    - Javadoc Update Turbine User manager mapping Fulcrum -> Turbine
    git-svn-id: https://svn.apache.org/repos/asf/turbine/fulcrum/trunk/security@1790413 13f79535-47bb-0310-9956-ffa450edef68

--- api/src/java/org/apache/fulcrum/security/model/turbine/TurbineUserManager.java
+++ api/src/java/org/apache/fulcrum/security/model/turbine/TurbineUserManager.java
@@ -20,11 +20,29 @@ package org.apache.fulcrum.security.model.turbine;
  */
 import org.apache.fulcrum.security.UserManager;
 import org.apache.fulcrum.security.entity.User;
+import org.apache.fulcrum.security.model.turbine.entity.TurbineUser;
 import org.apache.fulcrum.security.util.UnknownEntityException;
 
 /**
  * This interface describes the methods that glue the "turbine" model together.
- *
+ * 
+ * This Fulcrum user manager is as used as a delegate in the default Turbine user manager.
+ * 
+ * The user &lt;T extends {@link User}&gt; is wrapped using an extended user model, 
+ * which includes at least {@link TurbineUser} interface.
+ *  
+ * This interface´s methods are wrapped in Turbine user manager (org.apache.turbine.services.security.UserManager) either in a method with the same name (and very similar signature) 
+ * or mapped to method names as listed below:
+ * 
+ * <ul>
+ * <li>Turbine (framework) user manager method(s) -> method(s) in this (Fulcrum )manager 
+ * <li>createAccount -> {@link #addUser(User, String)}
+ * <li>removeAccount -> {@link #removeUser(User)}
+ * <li>store -> {@link #saveUser(User)}
+ * <li>retrieve (2x)-> {@link #getUser(String)}, {@link #getUser(String, String)}
+ * <li>retrieveList -> {@link #getAllUsers()}
+ * <li>accountExists (2x)-> {@link #checkExists(String)}, {@link #checkExists(User)}
+ * 
  * @author <a href="mailto:epugh@upstate.com">Eric Pugh</a>
  * @version $Id$
  */
@@ -34,7 +52,7 @@ public interface TurbineUserManager extends UserManager
      * Constructs an User object to represent an anonymous user of the
      * application.
      *
-     * @return An anonymous Turbine User.
+     * @return An anonymous Turbine User. 
      * @throws UnknownEntityException
      *             if the anonymous User object couldn't be constructed.
      */
