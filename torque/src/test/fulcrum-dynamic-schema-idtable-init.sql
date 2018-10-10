bfe07e0782a074353844e40010713cbc01298999
Jeffery Painter <painter@apache.org>
2018-10-10 16:38:43 +0000

    This _may_ help with upgrading to hsqldb 2.x, doesn't hurt with current build
    git-svn-id: https://svn.apache.org/repos/asf/turbine/fulcrum/trunk/security@1843471 13f79535-47bb-0310-9956-ffa450edef68

--- torque/src/test/fulcrum-dynamic-schema-idtable-init.sql
+++ torque/src/test/fulcrum-dynamic-schema-idtable-init.sql
@@ -14,11 +14,11 @@
 -- KIND, either express or implied.  See the License for the
 -- specific language governing permissions and limitations
 -- under the License.
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1101, 'FULCRUM_DYNAMIC_PERMISSION', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1102, 'FULCRUM_DYNAMIC_ROLE', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1103, 'FULCRUM_DYNAMIC_GROUP', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1104, 'FULCRUM_DYNAMIC_USER', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1105, 'DYNAMIC_ROLE_PERMISSION', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1106, 'DYNAMIC_USER_GROUP', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1107, 'DYNAMIC_GROUP_ROLE', 1000, 10);
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (1108, 'DYNAMIC_USER_DELEGATES', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1101, 'FULCRUM_DYNAMIC_PERMISSION', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1102, 'FULCRUM_DYNAMIC_ROLE', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1103, 'FULCRUM_DYNAMIC_GROUP', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1104, 'FULCRUM_DYNAMIC_USER', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1105, 'DYNAMIC_ROLE_PERMISSION', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1106, 'DYNAMIC_USER_GROUP', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1107, 'DYNAMIC_GROUP_ROLE', 1000, 10);
+insert into ID_TABLE (ID_TABLE_ID, TABLE_NAME, NEXT_ID, QUANTITY) VALUES (1108, 'DYNAMIC_USER_DELEGATES', 1000, 10);
